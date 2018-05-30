f99678baa5909ebbed3539f1e2803794de58424c
zhiqiang.zhou <zhouzhq7@gmail.com>
2018-05-30 14:42:10 +0100

    Implement data collection

--- binanceParser.py
+++ binanceParser.py
@@ -1,33 +1,53 @@
 import requests
 import re
+import os
+import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
 base_url = "https://api.binance.com"
 klines_query_api = "/api/v1/klines"
+price_query_api = "/api/v3/ticker/price"
+default_save_dir = "save"
+
+KLINE_INTERVAL_1MINUTE = '1m'
+KLINE_INTERVAL_3MINUTE = '3m'
+KLINE_INTERVAL_5MINUTE = '5m'
+KLINE_INTERVAL_15MINUTE = '15m'
+KLINE_INTERVAL_30MINUTE = '30m'
+KLINE_INTERVAL_1HOUR = '1h'
+KLINE_INTERVAL_2HOUR = '2h'
+KLINE_INTERVAL_4HOUR = '4h'
+KLINE_INTERVAL_6HOUR = '6h'
+KLINE_INTERVAL_8HOUR = '8h'
+KLINE_INTERVAL_12HOUR = '12h'
+KLINE_INTERVAL_1DAY = '1d'
+KLINE_INTERVAL_3DAY = '3d'
+KLINE_INTERVAL_1WEEK = '1w'
+KLINE_INTERVAL_1MONTH = '1M'
+
+def get_prices():
+    response = requests.get(base_url + price_query_api)
+
+    if response.status_code == 200:
+        data = response.content
+        data = data.decode('utf-8')
+        return data
+
+    else:
+        raise Exception("Data collection failed. Failed code : " + str(response.status_code))
 
 def process_kline_data(contents):
     contents = contents.decode('utf-8')
     data = re.split('\[*\]\,', contents)
-    ret = {}
-    ret['open_time'] = []
-    ret['open_datetime'] = []
-    ret['open'] = []
-    ret['high'] = []
-    ret['low'] = []
-    ret['close'] = []
-    ret['close_time'] = []
+    ret = []
     for d in data:
         tmp = d.replace('[', '')
+        tmp = tmp.replace(']', '')
         tmp = tmp.replace('\"', '')
         tmp = tmp.split(',')
-        ret['open_time'].append(int(tmp[0]))
-        ret['open_datetime'].append(datetime.fromtimestamp(int(tmp[0])/1000))
-        ret['open'].append(float(tmp[1]))
-        ret['high'].append(float(tmp[2]))
-        ret['low'].append(float(tmp[3]))
-        ret['close'].append(float(tmp[4]))
-        ret['close_time'].append(int(tmp[6]))
+        #tmp = [str(datetime.fromtimestamp(int(tmp[0])/1000))] + tmp
+        ret.append(tmp)
 
     return ret
 
@@ -68,18 +88,63 @@ def get_klines(symbol, interval, startTime=None, endTime=None):
     else:
         raise Exception("Data collection failed. Failed code : " + str(response.status_code))
 
+def save_data_to_csv(symbol, interval, data):
+    save_dir = os.path.join(default_save_dir, symbol)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    csv_file_name = symbol+'_'+interval+".csv"
+    csv_file_path = os.path.join(save_dir, csv_file_name)
+
+    with open(csv_file_path, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+
+def csv_file_write_headers(symbol, interval):
+    save_dir = os.path.join(default_save_dir, symbol)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    csv_file_name = symbol + '_' + interval + ".csv"
+    csv_file_path = os.path.join(save_dir, csv_file_name)
+
+    if os.path.isfile(csv_file_path):
+        os.remove(csv_file_path)
+    header = [['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time',
+              'Quote asset volume', 'Number of trades', 'Taker buy base asset volume',
+              'Taker buy quote asset volume', 'Ignore']]
+    with open(csv_file_path, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerows(header)
+
+def interval_to_millisecond(interval):
+    if interval == KLINE_INTERVAL_1MINUTE:
+        return 60000
+    elif interval == KLINE_INTERVAL_15MINUTE:
+        return 15*60*1000
+    elif interval == KLINE_INTERVAL_2HOUR:
+        return 2*60*60*1000
+    else:
+        raise Exception(interval + " is not supported in current version")
+
 def main():
+
     symbol = "BTCUSDT"
-    interval = "6h"
-    startTime = 1509940000000
-    endTime = 1579040000000
-    data = get_klines(symbol, interval, startTime)
-    print (len(data['open_time']))
-    plt.plot(data['open_datetime'], data['open'], label=symbol)
-    plt.ylabel(symbol+"($)")
-    plt.xticks(rotation=45)
-    plt.legend()
-    plt.show()
+    interval = "1m"
+
+    startTime = 1495000000000
+    csv_file_write_headers(symbol, interval)
+    while True:
+        print ("Collecting data on date : " + str(datetime.fromtimestamp(startTime/1000)))
+        data = get_klines(symbol, interval, startTime)
+        startTime = int(data[-1][0]) + interval_to_millisecond(interval)
+        save_data_to_csv(symbol,interval, data)
+        if len(data) < 1000:
+            print ("The last data collected is on date : " + str(datetime.fromtimestamp(int(data[-1][0])/1000)))
+            break
+
+
+
     return None
 
 if __name__=="__main__":
